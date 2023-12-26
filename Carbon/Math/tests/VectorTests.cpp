58baca7254e14fd06c51dabe6216db04edbfe252
Joseph Samyn <joseph.j.samyn@gmail.com>
2023-12-26 13:44:01 -0700

    Added unit tests projects to all libraries.

---
+++ Carbon/Math/tests/VectorTests.cpp
@@ -0,0 +1,8 @@
+
+#include <gtest/gtest.h>
+#include "Math.hpp"
+
+TEST(VectorTests, TestingSampleVector)
+{
+  EXPECT_EQ(1, 1);
+}
---
+++ Carbon/Renderer/tests/RendererTests.cpp
@@ -0,0 +1,6 @@
+#include <gtest/gtest.h>
+
+TEST(RendererTests, TestingRendererTests)
+{
+  EXPECT_EQ(1, 1);
+}
