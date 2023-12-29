e8b15d55a2b157de341461908365cb7a2ea5c246
Joseph Samyn <joseph.j.samyn@gmail.com>
2023-12-28 21:47:16 -0700

    Integrating SDL2 into project.

--- TestGame/src/Main.cpp
+++ TestGame/src/Main.cpp
@@ -1,9 +1,14 @@
-
+#define SDL_MAIN_HANDLED
 #include <stdio.h>
 #include <Math.hpp>
+#include <SDL2/SDL.h>
 
 int main(int argc, char* argv[])
 {
   printf("Hello Carbon");
+
+  // Testing SDL 
+  SDL_Init(SDL_INIT_EVERYTHING);
+  SDL_Quit();
   return 0;
 }
